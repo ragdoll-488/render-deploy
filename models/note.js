7f09053498419a3be1b3bc3586b1341b6fbcb0cb
ragdoll-488 <139529744+ragdoll-488@users.noreply.github.com>
2024-10-08 17:35:08 +0700

    Add files via upload

---
+++ models/note.js
@@ -0,0 +1,34 @@
+
+import mongoose from "mongoose";
+import env from "dotenv";
+
+env.config();
+
+const url = process.env.MONGODB_URI
+// console.log('connecting to', url)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+export default Note;
